9da834ec1ce644641f75528a5d7c9bce6a03716a
Andy <andy.gallay@polymtl.ca>
2023-12-16 17:06:24 -0500

    Initial Commit

---
+++ OpenPomodoro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OpenPomodoro
+//
+//  Created by Andy GALLAY on 2023-12-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OpenPomodoro/OpenPomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenPomodoroApp.swift
+//  OpenPomodoro
+//
+//  Created by Andy GALLAY on 2023-12-16.
+//
+
+import SwiftUI
+
+@main
+struct OpenPomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
